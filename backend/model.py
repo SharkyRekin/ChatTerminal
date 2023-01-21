03b2a23dbbd7e2a69422eee2b182a9d18c9c2691
alexiscampan <alexis.campan@ut-capitole.fr>
2023-01-21 10:17:49 +0100

    [NEW] added login backend

---
+++ backend/model.py
@@ -0,0 +1,29 @@
+import transformers
+from transformers import BloomForCausalLM
+from transformers import BloomTokenizerFast
+import torch
+
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m")
+tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+
+prompt = "It was a dark and stormy night"
+result_length = 50
+inputs = tokenizer(prompt, return_tensors="pt")
+
+print(tokenizer.decode(model.generate(inputs["input_ids"], 
+                           max_length=result_length
+                                                 )[0]))
+
+print(tokenizer.decode(model.generate(inputs["input_ids"],
+                        max_length=result_length, 
+                        num_beams=2, 
+                        no_repeat_ngram_size=2,
+                        early_stopping=True
+                        )[0]))
+
+print(tokenizer.decode(model.generate(inputs["input_ids"],
+                      max_length=result_length, 
+                      do_sample=True, 
+                      top_k=50, 
+                      top_p=0.9
+                      )[0]))
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+	{
+	path: 'tutulutu',
+	name: 'tkt',
+	component: () => import(/* webpackChunkName: "shark" */ '@/views/Shark.vue'),
+	},
     ],
   },
 ]
